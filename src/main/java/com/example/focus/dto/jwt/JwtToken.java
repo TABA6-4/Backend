02cfc7ffb086dd7c69d3297ffd1c179d1a7bf7b0
snvnn <yunh1205@gmail.com>
2024-12-12 21:26:59 +0900

    JWT Correction

--- src/main/java/com/example/focus/dto/jwt/JwtToken.java
+++ src/main/java/com/example/focus/dto/jwt/JwtToken.java
@@ -7,8 +7,9 @@ import lombok.Data;
 @Builder
 @Data
 @AllArgsConstructor
-public class JwtToken {     //클라이언트에 토큰을 보내기 위함
+public class JwtToken {     // 클라이언트에 토큰을 보내기 위함
     private String grantType;
     private String accessToken;
     private String refreshToken;
+    private Long userId; // user_id 추가
 }
